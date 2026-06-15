fb67a80c2303c1685a970924044f96e65fc89ae2
Mpakistan <mpakistan56thr@gmail.com>
2026-06-15 22:30:16 +0300

    Create database

--- db_connect.php
+++ db_connect.php
@@ -7,9 +7,16 @@ $dbname = "cyberDB";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if($conn){
-    echo "Connected successfully";
-}else{
-    echo "Connection failed: " . mysqli_connect_error();
+$sql = mysql_query($conn, "CREATE TABLE users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    password VARCHAR(255) NOT NULL,
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);");
+if ($sql) {
+    echo "Table users created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
 }
 ?>
