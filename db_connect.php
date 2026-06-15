df5d845cd8c0cdf313ba47d428f4319f73e9ff7d
Mpakistan <mpakistan56thr@gmail.com>
2026-06-16 00:05:49 +0300

    Database connection

--- db_connect.php
+++ db_connect.php
@@ -7,16 +7,16 @@ $dbname = "cyberDB";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-$sql = mysql_query($conn, "CREATE TABLE users (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    password VARCHAR(255) NOT NULL,
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);");
-if ($sql) {
-    echo "Table users created successfully";
-} else {
-    echo "Error creating table: " . mysqli_error($conn);
-}
+// $sql = mysqli_query($conn, "CREATE TABLE incidents (
+//     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+//     incident_title VARCHAR(255) NOT NULL,
+//     incident_type VARCHAR(255) NOT NULL,
+//     incident_description TEXT,
+//     incident_date DATE NOT NULL
+// );");
+// if ($sql) {
+//     echo "Table users created successfully";
+// } else {
+//     echo "Error creating table: " . mysqli_error($conn);
+// }
 ?>
